2aa8f6d28e5e60d390661acbe82ad1c64fa044ea
Nosipho <manyasenosipho@gmail.com>
2023-03-08 21:22:48 +0200

    add tailwind css

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,10 @@ import './globals.css'
 export const metadata = {
   title: 'Google Clone next js 13',
   description: 'Google clone by create Next js 13',
+  icons: {
+    icon: [{url: '/favicon.svg', type: 'image/svg'}]
+  }
+
 }
 
 export default function RootLayout({ children }) {
