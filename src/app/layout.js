3e78fe3cbcc24058b7d1e7fce78eaedbe13440d2
Nosipho <manyasenosipho@gmail.com>
2023-03-10 09:24:44 +0200

    add footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer'
 import './globals.css'
 
 export const metadata = {
@@ -12,7 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        
+        {children}
+        
+        <Footer />
+      </body>
     </html>
   )
 }
